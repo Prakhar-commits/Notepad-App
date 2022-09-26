51a2aae7123bb861e40cd330bf7388205ee4469e
Prakhar <prakharb56@gmail.com>
2022-09-26 16:19:37 +0530

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+const bold = document.querySelector(".bold-btn");
+const italic = document.querySelector(".italic-btn");
+const reset = document.querySelector(".reset-btn");
+const save = document.querySelector(".save-btn");
+const add = document.querySelector(".add-btn");
+
+
+let title = document.querySelector
